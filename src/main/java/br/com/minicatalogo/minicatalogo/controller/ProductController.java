c0fd6d04b658343ffbcc00fed85f8656f3ddc583
SiroiluT <tulio-evangelista@outlook.com>
2026-04-08 23:33:21 -0300

    feat cria rotas de buscar,criar,editar e deletar

--- src/main/java/br/com/minicatalogo/minicatalogo/controller/ProductController.java
+++ src/main/java/br/com/minicatalogo/minicatalogo/controller/ProductController.java
@@ -1,11 +1,11 @@
 package br.com.minicatalogo.minicatalogo.controller;
 
 
+import br.com.minicatalogo.minicatalogo.model.ProductModel;
 import br.com.minicatalogo.minicatalogo.service.ProductService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -14,14 +14,44 @@ public class ProductController {
 
     ProductService productService;
 
-        public ProductController(ProductService productService) {
-            this.productService = productService;
-        }
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
 
     @PostMapping("/create")
-    public String createProduct(@RequestBody String product) {
-        return "Produto criado com sucesso!";
+    public ResponseEntity<String> createProduct(@RequestBody ProductModel productModel) {
+        {
+            return ResponseEntity.status(HttpStatus.CREATED).body("Produto criado com sucesso: ");
+
+        }
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable Long id ,@RequestBody ProductModel productModel) {
+        {
+            return ResponseEntity.status(HttpStatus.OK).body("Produto atualizado com sucesso: ");
+
+        }
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<String> getAllProducts() {
+        {
+            return ResponseEntity.status(HttpStatus.OK).body("Lista de produtos: ");
+
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        {
+            return ResponseEntity.status(HttpStatus.OK).body("Produto deletado com sucesso: ");
+
+        }
+    }
+
+
+
 
 }
